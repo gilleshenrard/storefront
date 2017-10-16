53928aab64507a9037d45c0a12d003302b676ef5
gilleshenrard <gilles.henrard@hotmail.com>
2017-10-16 22:37:31 +0200

    migrated to php files + split index into views

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+	<html lang="en">
+	
+	<head>
+		
+		<meta charset="utf-8">
+		<meta name="viewport"
+			content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		
+		<title>Creative - Start Bootstrap Theme</title>
+		
+		<!-- Bootstrap core CSS -->
+		<link href="views/vendor/bootstrap/css/bootstrap.min.css"
+			rel="stylesheet">
+		
+		<!-- Custom fonts for this template -->
+		<link href="views/vendor/font-awesome/css/font-awesome.min.css"
+			rel="stylesheet" type="text/css">
+		<link
+			href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+			rel='stylesheet' type='text/css'>
+		<link
+			href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+			rel='stylesheet' type='text/css'>
+		
+		<!-- Plugin CSS -->
+		<link href="views/vendor/magnific-popup/magnific-popup.css"
+			rel="stylesheet">
+		
+		<!-- Custom styles for this template -->
+		<link href="views/css/creative.min.css" rel="stylesheet">
+	
+	</head>
+	
+	<body id="page-top">
+	
+		<?php include "views/menu-header-footer/menu.php";?>
+		
+		<?php include 'views/menu-header-footer/header.php';?>
+	
+		<?php include 'views/about.php';?>
+	
+		<?php include 'views/services.php';?>
+	
+		<?php include 'views/portfolio.php';?>
+	
+		<?php include 'views/menu-header-footer/footer.php';?>
+	
+		<!-- Bootstrap core JavaScript -->
+		<script src="views/vendor/jquery/jquery.min.js"></script>
+		<script src="views/vendor/popper/popper.min.js"></script>
+		<script src="views/vendor/bootstrap/js/bootstrap.min.js"></script>
+	
+		<!-- Plugin JavaScript -->
+		<script src="views/vendor/jquery-easing/jquery.easing.min.js"></script>
+		<script src="views/vendor/scrollreveal/scrollreveal.min.js"></script>
+		<script src="views/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+	
+		<!-- Custom scripts for this template -->
+		<script src="views/js/creative.min.js"></script>
+	
+	</body>
+
+</html>
