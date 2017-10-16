f42d4a1cdcbfcc2cb8ede4e41986448ebb581a32
gilleshenrard <gilles.henrard@hotmail.com>
2017-10-17 00:37:13 +0200

    change interests content

--- views/interests.php
+++ views/interests.php
@@ -1,8 +1,8 @@
-		<section id="services">
+		<section id="interests">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12 text-center">
-						<h2 class="section-heading">At Your Service</h2>
+						<h2 class="section-heading">What I'm into</h2>
 						<hr class="primary">
 					</div>
 				</div>
@@ -10,35 +10,31 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-3 col-md-6 text-center">
-						<div class="service-box">
-							<i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
-							<h3>Sturdy Templates</h3>
-							<p class="text-muted">Our templates are updated regularly so
-								they don't break.</p>
+						<div class="interest-box">
+							<i class="fa fa-4x fa-code text-primary sr-icons"></i>
+							<h3>Coding</h3>
+							<p class="text-muted">So many languages to try out and learn from</p>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 text-center">
-						<div class="service-box">
-							<i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
-							<h3>Ready to Ship</h3>
-							<p class="text-muted">You can use this theme as is, or you can
-								make changes!</p>
+						<div class="interest-box">
+							<i class="fa fa-4x fa-microchip text-primary sr-icons"></i>
+							<h3>Technology</h3>
+							<p class="text-muted">Constantly discover the wonders the human brain can do</p>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 text-center">
-						<div class="service-box">
-							<i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
-							<h3>Up to Date</h3>
-							<p class="text-muted">We update dependencies to keep things
-								fresh.</p>
+						<div class="interest-box">
+							<i class="fa fa-4x fa-music text-primary sr-icons"></i>
+							<h3>Music</h3>
+							<p class="text-muted">I crave trying each and every instrument I come across</p>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 text-center">
-						<div class="service-box">
-							<i class="fa fa-4x fa-heart text-primary sr-icons"></i>
-							<h3>Made with Love</h3>
-							<p class="text-muted">You have to make your websites with love
-								these days!</p>
+						<div class="interest-box">
+							<i class="fa fa-4x fa-wrench text-primary sr-icons"></i>
+							<h3>Handworking</h3>
+							<p class="text-muted">Trying to improve my handcrafting skills</p>
 						</div>
 					</div>
 				</div>
