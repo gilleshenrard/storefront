e4afb647ea534e41ff66ecf6ae52326bc12f6d92
gilleshenrard <gilles.henrard@hotmail.com>
2017-10-17 00:37:39 +0200

    improve about section

--- views/about.php
+++ views/about.php
@@ -2,15 +2,12 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-8 mx-auto text-center">
-						<h2 class="section-heading text-white">We've got what you
-							need!</h2>
+						<h2 class="section-heading text-white">About me</h2>
 						<hr class="light">
-						<p class="text-faded">Start Bootstrap has everything you need
-							to get your new website up and running in no time! All of the
-							templates and themes on Start Bootstrap are open source, free to
-							download, and easy to use. No strings attached!</p>
+						<p class="text-faded">Easy going and adaptative, I easily find interest in new projects and challenges. 
+						My most valuable moments are when I have the chance to let my mind wander around tips and tricks to create something the most elegant way possible.</p>
 						<a class="btn btn-default btn-xl js-scroll-trigger"
-							href="#services">Get Started!</a>
+							href="#interests">See what I like</a>
 					</div>
 				</div>
 			</div>
