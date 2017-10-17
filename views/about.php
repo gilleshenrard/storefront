d22b9b8980a73f6bc8d1e65b33e9854f1c4d3c4c
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-17 16:02:58 +0200

    change font colour

--- views/about.php
+++ views/about.php
@@ -2,7 +2,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-8 mx-auto text-center">
-						<h2 class="section-heading text-white">About me</h2>
+						<h2 class="section-heading text-light">About me</h2>
 						<hr class="light">
 						<p class="text-faded">Easy going and adaptative, I easily find interest in new projects and challenges. 
 						My most valuable moments are when I have the chance to let my mind wander around tips and tricks to create something the most elegant way possible.</p>
