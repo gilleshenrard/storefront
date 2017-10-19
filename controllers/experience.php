e5e4b832479676b1510183e4060cd288b77a30f9
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-19 12:07:54 +0200

    Simplify information

--- controllers/experience.php
+++ controllers/experience.php
@@ -6,7 +6,8 @@ $experiences = [
         'contractor' => "Bureau van Dijk",
         'period' => "Jan 2017 - Present",
         'location' => "Brussels, Belgium",
-        'description' => "2nd line support for the FACT (previously Bankscore) application.",
+        'languages' => ["FR", "EN"],
+        'description' => "Second line support for the web-based FACT application.",
         'technologies' => [
             'Microsoft IIS',
             'Microsoft SQL Server 2008, 2012, 2014',
@@ -26,8 +27,8 @@ $experiences = [
         'company' => "Court of Justice of the European Union",
         'period' => "Nov 2015 - Dec 2016",
         'location' => "Luxembourg City, Luxembourg",
-        'description' => "First level service desk agent at the European Court of Justice.
-						  The support is provided in English and French.",
+        'languages' => ["FR"],
+        'description' => "First and second level service desk agent.",
         'technologies' => [
             'Microsoft Active Directory',
             'Microsoft File server',
@@ -40,37 +41,34 @@ $experiences = [
     'catella' => [
         'job' => 'Service Desk Operator',
         'company' => "Catella Bank",
+        'contractor' => "CTG",
         'period' => "Jun 2015 - Oct 2015",
         'location' => "Capellen, Luxembourg",
-        'description' => "Service desk agent at Catella Bank (private investments bank)
-						  The support is provided in English and French.",
-        'responsibilities' => "- Help users with general and more specific issues with their Windows session, phone, tools and requests.
-                                            - Help outsourcing the helpdesk service to CTG, in order to provide a call center coverage.
-                                            - Set up standard templates, build procedures
-                                            - Review the evolution of the infrastructure and report the eventual problems we could encounter with the system to the IT team.
-                                            - Propose hardware oriented support, such as placing server blades, cabling, conference rooms setup, ...",
+        'languages' => ["FR", "EN"],
+        'description' => "Service desk agent at Catella Bank (private investments bank)",
+        'responsibilities' => ["Helpdesk outsourcing to CTG",
+                               "Documentation and standardisation",
+                               "Second line system administration",
+                               "Software and hardawe support"],
         'technologies' => [
             'Microsoft Active Directory',
             'Microsoft File server',
-            'Sysprep / Windows ADK',
             'Citrix (XenDesktop, XenServer, XenApp)',
-            'Blackshield',
             'IBM Notes/Domino and Admin',
             'Jira',
             'ScopTel',
-            'Xerox Center',
             'Eset Nod32 server'
         ]
     ],
     
     
     'adidas' => [
-        'job' => 'Helpdesk Support',
+        'job' => 'Customer Support',
         'company' => "Adidas",
         'period' => "Jun 2014 - Mar 2015",
         'location' => "Maastricht, Netherlands",
-        'description' => "Customer support for the Adidas online shop, for France, Belgium, UK, Denmark, Finland and Sweden.
-							The support is provided in English and French."
+        'languages' => ["FR", "EN"],
+        'description' => "Support for the Adidas online shop, helping customers from France, Belgium, UK, Ireland, Denmark, Finland and Sweden"
     ]
 ];
 
