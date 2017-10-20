7e2520d9415d67e5013409eb0e30527180ab1b48
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-20 10:01:53 +0200

    add type to js and css

--- index.php
+++ index.php
@@ -3,40 +3,30 @@
 	
 	<head>
 		
-		<meta charset="utf-8">
-		<meta name="viewport"
-			content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		
 		<title>Storefront - Gilles Henrard</title>
 		
 		<!-- Bootstrap core CSS -->
-		<link href="views/vendor/bootstrap/css/bootstrap.min.css"
-			rel="stylesheet">
+		<link href="views/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 		
 		<!-- Custom fonts for this template -->
-		<link href="views/vendor/font-awesome/css/font-awesome.min.css"
-			rel="stylesheet" type="text/css">
-		<link
-			href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
-			rel='stylesheet' type='text/css'>
-		<link
-			href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
-			rel='stylesheet' type='text/css'>
+		<link href="views/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css' />
+		<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css' />
 		
 		<!-- Plugin CSS -->
-		<link href="views/vendor/magnific-popup/magnific-popup.css"
-			rel="stylesheet">
+		<link href="views/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css" />
 		
 		<!-- Custom styles for this template -->
-		<link href="views/css/global.css" rel="stylesheet">
-		<link href="views/css/menu.css" rel="stylesheet" />
-		<link href="views/css/header.css" rel="stylesheet" />
-		<link href="views/css/interests.css" rel="stylesheet" />
-		<link href="views/css/timeline.css" rel="stylesheet" />
-		<link href="views/css/experience.css" rel="stylesheet" />
-		<link href="views/css/skills.css" rel="stylesheet" />
+		<link href="views/css/global.css" rel="stylesheet" type="text/css" />
+		<link href="views/css/menu.css" rel="stylesheet" type="text/css" />
+		<link href="views/css/header.css" rel="stylesheet" type="text/css" />
+		<link href="views/css/interests.css" rel="stylesheet" type="text/css" />
+		<link href="views/css/timeline.css" rel="stylesheet" type="text/css" />
+		<link href="views/css/experience.css" rel="stylesheet" type="text/css" />
+		<link href="views/css/skills.css" rel="stylesheet" type="text/css" />
 	
 	</head>
 	
@@ -55,17 +45,17 @@
 		<?php include 'controllers/contact.php';?>
 	
 		<!-- Bootstrap core JavaScript -->
-		<script src="views/vendor/jquery/jquery.min.js"></script>
-		<script src="views/vendor/popper/popper.min.js"></script>
-		<script src="views/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="views/vendor/jquery/jquery.min.js" type="application/javascript"></script>
+		<script src="views/vendor/popper/popper.min.js" type="application/javascript"></script>
+		<script src="views/vendor/bootstrap/js/bootstrap.min.js" type="application/javascript"></script>
 	
 		<!-- Plugin JavaScript -->
-		<script src="views/vendor/jquery-easing/jquery.easing.min.js"></script>
-		<script src="views/vendor/scrollreveal/scrollreveal.min.js"></script>
-		<script src="views/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="views/vendor/jquery-easing/jquery.easing.min.js" type="application/javascript"></script>
+		<script src="views/vendor/scrollreveal/scrollreveal.min.js" type="application/javascript"></script>
+		<script src="views/vendor/magnific-popup/jquery.magnific-popup.min.js" type="application/javascript"></script>
 	
 		<!-- Custom scripts for this template -->
-		<script src="views/js/global.js"></script>
+		<script src="views/js/global.js" type="application/javascript"></script>
 	
 	</body>
 
