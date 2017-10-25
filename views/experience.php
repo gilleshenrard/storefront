763d8e246f90fda48ccf5f0c4c9c7f3296c7088e
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-25 23:35:39 +0200

    use another timeline template

--- views/experience.php
+++ views/experience.php
@@ -4,6 +4,10 @@
 					<div class="col-lg-8 mx-auto text-center">
 						<h2 class="section-heading text-light">What I've done so far</h2>
 						<hr class="light">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-8 mx-auto text-center">
         				<ul class="timeline">
         					<?php foreach ($experiences as $company => $info){
         					    if ($inverted)
@@ -16,7 +20,13 @@
         					    echo "</li>";
         					    $inverted = !$inverted;
                             }?>
-                            <li><div class="timeline-badge bg-light" id="timeline-begin"><i class="fa fa-info"></i></div></li>
+    						<li class="timeline-inverted">
+    							<div class="timeline-image bg-light">
+    								<h4 class="text-primary">
+    									Be Part <br>Of Our <br>Story!
+    								</h4>
+    							</div>
+    						</li>
         				</ul>
         			</div>
 				</div>
--- views/timeline-panel.php
+++ views/timeline-panel.php
@@ -1,25 +1,28 @@
-        						<div class="timeline-badge" style="background: url('views/img/logo/<?php echo $company;?>.png') no-repeat #fff center center; background-size: contain">
-        						</div>
+        						<!--div class="timeline-badge" style="background: url('views/img/logo/<?php echo $company;?>.png') no-repeat #fff center center; background-size: contain">
+        						</div-->
+								<div class="timeline-image bg-light">
+    								<img class="rounded-circle img-fluid" src="views/img/logo/<?php echo $company;?>.png" alt="">
+    							</div>
         						<div class="timeline-panel">
-        							<div class="timeline-heading">
+        							<div class="timeline-heading text-light">
         								<h4 class="timeline-title"><?php echo $info['job'];?></h4>
         								<h5><strong><?php echo $info['company'];?></strong><?php if (isset($info['contractor'])) echo " - ".$info['contractor'];?></h5>
         								<p>
-        									<small class="text-muted"><i class="fa fa-calendar-o"></i>
+        									<small><i class="fa fa-calendar-o"></i>
         										<?php echo $info['period'];?></small>
         									<br/>
-        									<small class="text-muted"><i class="fa fa-map-o"></i>
+        									<small><i class="fa fa-map-o"></i>
         										<?php echo $info['location'];?></small>
         									<br/>
-        									<small class="text-muted"><i class="fa fa-comments-o"></i>
+        									<small><i class="fa fa-comments-o"></i>
         										<?php foreach ($info['languages'] as $lang){
         										    echo $lang."  ";
         										}?></small>
         								</p>
         							</div>
-        							<div class="timeline-body">
+        							<div class="timeline-body text-light text-center">
         								<?php if (isset($info['responsibilities']) or isset($info['technologies']) or isset($info['description'])){
-        								    echo "<i class='fa fa-2x fa-plus-circle text-primary sr-icons' id='btn-".$company."' data-toggle='collapse' data-target='#".$company."'></i>
+        								    echo "<i class='fa fa-2x fa-plus-circle text-light sr-icons' id='btn-".$company."' data-toggle='collapse' data-target='#".$company."'></i>
                                                   <div class='collapse' id='".$company."'>";
         								    
         								    if (isset($info['description'])){
@@ -37,7 +40,7 @@
         								    }
         								    
         								    if (isset($info['technologies'])){
-        								        echo "<hr class='primary'>";
+        								        echo "<hr class='light'>";
         								        echo "<p>Technologies used</p>";
         								        echo "<ul>";
         								        foreach ($info['technologies'] as $tech){
