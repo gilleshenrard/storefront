d26fa30ee2709e62e5da7caf1b1f218efea11e3a
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-25 18:40:41 +0200

    remove contact form

--- controllers/contact.php
+++ controllers/contact.php
@@ -1,13 +1,3 @@
 <?php
-$opt = array(
-    'subject' => FILTER_SANITIZE_STRING,
-    'msg' => FILTER_SANITIZE_STRING,
-    'email' => FILTER_SANITIZE_EMAIL,
-);
-$post = filter_input_array(INPUT_POST, $opt);
-
-if (!is_null($post)){
-    //ok
-}
 
 include 'views/contact.php';
