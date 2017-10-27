417c3fcf70d5d649aa825fa27cb370dca18a27e9
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-27 16:56:34 +0200

    add sections to About

--- views/about.php
+++ views/about.php
@@ -6,9 +6,25 @@
 						<hr class="light">
 						<p class="text-faded">Easy going and adaptative, I easily find interest in new projects and challenges. 
 						My most valuable moments are when I have the chance to let my mind wander around tips and tricks to create something the most elegant way possible.</p>
-						<a class="btn btn-default btn-xl js-scroll-trigger"
-							href="#interests">See what I like</a>
 					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12 text-center text-light">
+					<i class="fa fa-5x fa-comment"></i>
+					<p>French</p>
+					<p>English</p><br />
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12 text-center text-light">
+					<i class="fa fa-5x fa-map-signs"></i>
+					<p>Born : Liege, Belgium<p>
+					<p>Located : Brussels, Belgium<p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 mx-auto text-center">
+					<a class="btn btn-default btn-xl js-scroll-trigger"
+					   href="#interests">See what I like</a>
 				</div>
 			</div>
 		</section>
