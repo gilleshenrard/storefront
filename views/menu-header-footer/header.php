dfd57df4d2794597fa7bdc71330d927d05d5fc50
Gilles Henrard <gilles.henrard@hotmail.com>
2017-10-31 16:41:37 +0100

    remove button in header

--- views/menu-header-footer/header.php
+++ views/menu-header-footer/header.php
@@ -4,7 +4,6 @@
 					<h1 id="homeHeading">Gilles Henrard</h1>
 					<hr>
 					<p>Fond of development, psyched on technology, passionate about music</p>
-					<a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Discover me</a>
 				</div>
 			</div>
 		</header>
