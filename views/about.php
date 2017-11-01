cc8d56732ee659f27cfb8e5ee4e6bdcf7e92dd54
Gilles <gilles@gilles-dell.lan>
2017-11-01 22:13:51 +0100

    improve google static map implementation

--- views/about.php
+++ views/about.php
@@ -1,31 +1,15 @@
 		<section class="bg-primary" id="about">
+            <div class="row">
+                <div id="curcity-map"></div>
+            </div>
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-8 mx-auto text-center">
 						<h2 class="section-heading text-light">About me</h2>
 						<hr class="light">
-						<p class="text-faded">Easy going and adaptative, I easily find interest in new projects and challenges. 
+						<p class="text-faded">Easy going and adaptative, I easily find interest in new projects and challenges.
 						My most valuable moments are when I have the chance to let my mind wander around tips and tricks to create something the most elegant way possible.</p>
 					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-4 col-md-4 col-sm-12 text-center text-light">
-					<i class="fa fa-5x fa-comment"></i>
-					<h4>French</h4>
-					<h4>English</h4>
-				</div>
-			</div>
-			<div class="row">
-				<img src="https://maps.googleapis.com/maps/api/staticmap?
-						key=AIzaSyBMGfYeDyC-blGyqgVFZDzkxvD5S1KM3_Q
-						&center=Brussels
-						&size=800x150
-						&scale=2
-						&zoom=6
-						&type=terrain
-						&style=feature:road|visibility:off
-						&markers=size:mid|Brussels"
-				     alt="Map" class="img-fluid" />
 			</div>
 		</section>
