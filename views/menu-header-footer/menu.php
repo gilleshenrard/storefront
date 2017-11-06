72fe36434c6fc374dd93a17589b641226638b15b
Gilles Henrard <gilles.henrard@hotmail.com>
2017-11-06 14:12:28 +0100

    Add 'beta' statement in brand

--- views/menu-header-footer/menu.php
+++ views/menu-header-footer/menu.php
@@ -2,7 +2,7 @@
 		<nav class="navbar navbar-expand-lg navbar-light fixed-top"
 			id="mainNav">
 			<div class="container">
-				<a class="navbar-brand js-scroll-trigger" href="#page-top">Gilles Henrard</a>
+				<a class="navbar-brand js-scroll-trigger" href="#page-top">Gilles Henrard <small class="text-primary">beta</small></a>
 				<button class="navbar-toggler navbar-toggler-right" type="button"
 					data-toggle="collapse" data-target="#navbarResponsive"
 					aria-controls="navbarResponsive" aria-expanded="false"
