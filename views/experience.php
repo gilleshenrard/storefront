9c754f81ec56d09e0496a230bb35d8802754bc31
Gilles Henrard <gilles.henrard@hotmail.com>
2017-11-08 23:18:05 +0100

    apply parallax to whole section

--- views/experience.php
+++ views/experience.php
@@ -1,5 +1,5 @@
-		<section id="experience">
-			<div class="container-fluid" data-stellar-background-ratio="0.5">
+		<section id="experience" data-stellar-background-ratio="0.65">
+			<div class="container-fluid">
 				<div class="row">
 					<div class="col-lg-8 mx-auto text-center">
 						<h2 class="section-heading text-light">What I've done so far</h2>
