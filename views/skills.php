4b4fa60bcc3712c1430ddcc4e492a44dfd67df0c
Gilles Henrard <gilles.henrard@hotmail.com>
2017-11-10 16:58:01 +0100

    fix reverse graph spreading

--- views/skills.php
+++ views/skills.php
@@ -14,15 +14,17 @@
         				<!--h3 class="text-center">Programming languages</h3-->
         			</div>
             		<div class="col-lg-6 col-md-10 col-sm-4 mx-auto">
-            			<ul class="bargraph text-left">
-				            <li><div class="graph-inter-h bg-light text-primary">Python</div></li>
-            				<li><div class="graph-pro-l bg-light text-primary">Django</div></li>
-            				<li><div class="graph-pro-l bg-light text-primary">C++</div></li>
-            				<li><div class="graph-pro-h bg-light text-primary">C#</div></li>
-            				<li><div class="graph-inter-l bg-light text-primary">Java</div></li>
-            				<li><div class="graph-adv-l bg-light text-primary">C</div></li>
-            				<li><div class="graph-inter-h bg-light text-primary">Powershell</div></li>
-            			</ul>
+			            <!--div class="container-fluid"-->
+	                        <ul class="bargraph text-left">
+					            <li><div class="graph-inter-h bg-light text-primary">Python</div></li>
+	                            <li><div class="graph-pro-l bg-light text-primary">Django</div></li>
+	                            <li><div class="graph-pro-l bg-light text-primary">C++</div></li>
+	                            <li><div class="graph-pro-h bg-light text-primary">C#</div></li>
+	                            <li><div class="graph-inter-l bg-light text-primary">Java</div></li>
+	                            <li><div class="graph-adv-l bg-light text-primary">C</div></li>
+	                            <li><div class="graph-inter-h bg-light text-primary">Powershell</div></li>
+	                        </ul>
+			            <!--/div-->
                     </div>
             		<div class="col-lg-4 mx-auto">
             		</div>
@@ -32,17 +34,19 @@
             		<div class="col-lg-4 mx-auto">
             		</div>
             		<div class="col-lg-6 col-md-10 col-sm-4 mx-auto">
-            			<ul class="bargraph bargraph-reverse text-right">
-            				<li><div class="graph-pro-l bg-light text-primary">Windows</div></li>
-            				<li><div class="graph-pro-l bg-light text-primary">Linux</div></li>
-            				<li><div class="graph-inter-l bg-light text-primary">Raspberry Pi</div></li>
-            				<li><div class="graph-inter-h bg-light text-primary">Arduino</div></li>
-            			</ul>
+			            <!--div class="container-fluid graph-reverse-container"-->
+	                        <ul class="bargraph bargraph-reverse text-right">
+	                            <li><div class="graph-pro-l bg-light text-primary">Windows</div></li>
+	                            <li><div class="graph-pro-l bg-light text-primary">Linux</div></li>
+	                            <li><div class="graph-inter-l bg-light text-primary">Raspberry Pi</div></li>
+	                            <li><div class="graph-inter-h bg-light text-primary">Arduino</div></li>
+	                        </ul>
+			            <!--/div-->
                     </div>
-        			<div class="col-lg-2 col-md-2 col-sm-4 mx-auto text-light text-center">
-        				<i class="fa fa-tv fa-5x skills-header sr-icon"></i>
-        				<!--h3 class="text-center">Programming languages</h3-->
-        			</div>
+			        <div class="col-lg-2 col-md-2 col-sm-4 mx-auto text-light text-center">
+				        <i class="fa fa-tv fa-5x skills-header sr-icon"></i>
+				        <!--h3 class="text-center">Programming languages</h3-->
+			        </div>
         		</div>
         	</div>
 		</section>
